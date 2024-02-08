2519545ac7248055cfbaa53f437b53a8a277295b
halajda <halajdabenj@gmail.com>
2024-02-08 14:25:32 +0100

    projet final

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -8,24 +8,8 @@ import Data from "../../data/logements.json"
 
 function Header() {
     let localisation = window.location.pathname;
+    console.log(localisation)
 	const [underline, setUnderline] = useState("accueil");
-    const id = Data.id
-    const [count, setCount] = useState(0);
-    const newRef = useRef(null);
-    useEffect(()=>{
-        document.addEventListener("mousedown",handleOutsideClick);
-        return () => {
-            document.removeEventListener("mousedown", handleOutsideClick)
-        };
-    });
-    const handleClick = () => {
-        setCount("autre");
-    };
-    const handleOutsideClick = (e) => {
-        if (newRef.current && !newRef.current.contains(e.target)){
-            setCount(("autre"))
-        }
-    };
 	return (
 		<header className='header'>
 			<h1>
@@ -38,9 +22,6 @@ function Header() {
                     </li>
                     <li className={localisation === '/a-propos' ? 'nav_list_active' : 'nav_item'} onClick={() => setUnderline("propos")}>
                         <Link to="/a-propos">A Propos</Link>    
-                    </li>
-                    <li className={localisation === '/*' ? 'nav_item' : 'nav_item'} onClick={() => setUnderline("autre")}>  
-                    <Link to="/*"></Link>  
                     </li>
                 </ul>
             </nav>
